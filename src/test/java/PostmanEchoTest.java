1009ef796047720f5d2506f6250224d432b2bf01
Nikita Kruglov <bybalesh234@gmail.com>
2023-06-20 22:55:32 +0600

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+
+        void shouldShowPostRequest() {
+            given()
+                    .baseUri("https://postman-echo.com")
+                    .contentType("text/plain; charset=UTF-8")
+                    .body("Data")
+                    .when()
+                    .post("/post")
+                    .then()
+                    .statusCode(200)
+                    .body("data", equalTo("Data"))
+            ;
+        }
+    }
